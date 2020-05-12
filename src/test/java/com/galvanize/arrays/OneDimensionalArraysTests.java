25eb11ca5dab78f89d10d8008d17df48aac349b3
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-05-11 21:05:09 -0700

    setup OICE

---
+++ src/test/java/com/galvanize/arrays/OneDimensionalArraysTests.java
@@ -0,0 +1,33 @@
+package com.galvanize.arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class OneDimensionalArraysTests {
+
+    //OICE - An effective way to consume requirements
+    //Output
+    //  integer values printed from array
+    //Input
+    //  integer value from stdin
+    //Constraints
+    //  Are there any stated constraints to the problem?
+    //      No
+    //  Are there any indicators of time vs space trade-offs?
+    //      No
+    //Examples and Edge Cases
+    //  List examples from problem
+    //  Think of edge cases at boundary values, 0 and 1 are typical
+
+    @Test
+    public void createArray_returnsArray() {
+        //Setup
+        OneDimensionalArray array = new OneDimensionalArray();
+
+
+        //Exercise
+
+        //Assert
+        assertEquals(expected, actual);
+        //Teardown
+    }
+}
