00f7556c1ddfe0d3d01fa6b0b8ee5a57a3a2cbd6
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-05-15 08:46:27 -0700

    test passing

--- src/main/java/com/galvanize/arraylist/ChallengeArrayList.java
+++ src/main/java/com/galvanize/arraylist/ChallengeArrayList.java
@@ -3,14 +3,14 @@ package com.galvanize.arraylist;
 public class ChallengeArrayList {
 
 
-
-    public static int getValueAtY(Integer array[]) {
-        int y = array[0];
+    public static void getValueAtXY(Integer[][] arrays, int x, int y) {
+        Integer array[];
+        array = arrays[x];
         if (array.length <= y){
             System.out.println("ERROR!");
-            return 0;
         }else{
-            return array[y];
+            String value = Integer.toString(array[y]);
+            System.out.println(value);
         }
     }
 }
--- src/test/java/com/galvanize/arraylist/ChallengeArrayListTest.java
+++ src/test/java/com/galvanize/arraylist/ChallengeArrayListTest.java
@@ -1,8 +1,18 @@
 package com.galvanize.arraylist;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 class ChallengeArrayListTest {
     //Alex Lundin
@@ -32,15 +42,53 @@ class ChallengeArrayListTest {
         //List<Integer> myList4 = Arrays.asList(array4);
         //List<Integer> myList5 = Arrays.asList(array5);
 
-    @Test
-    public void getValueAtX_returnsInt() {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+
+    @BeforeAll
+    public void setup() {
         //Setup
-        int expected = 44;
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream (errContent));
+    }
+
+    @AfterAll
+    public void restoreStreams(){
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+
+    @Test
+    public void getValueAtXY_printsInt() {
+        //Setup
+        String expected = "74";
+        Integer array0[] = {2};
         Integer array1[] = {5, 41, 77, 74, 22, 44};
+        Integer array2[] = {1, 12};
+        Integer arrays[][] = {array0, array1, array2};
         //Exercise
-        int actual = ChallengeArrayList.getValueAtY(array1);
+        ChallengeArrayList.getValueAtXY(arrays, 1, 3);
         //Assert
-        assertEquals(expected, actual);
+        assertThat(outContent.toString(), containsString(expected));
+        //Teardown
+    }
+
+    @Test
+    public void getValueAtXY_printsError() {
+        //Setup
+        String expected = "ERROR!";
+        Integer array0[] = {2};
+        Integer array1[] = {5};
+        Integer array2[] = {1};
+        Integer arrays[][] = {array0, array1, array2};
+        //Exercise
+        ChallengeArrayList.getValueAtXY(arrays, 1, 3);
+        //Assert
+        assertThat(outContent.toString(), containsString(expected));
         //Teardown
     }
 }
