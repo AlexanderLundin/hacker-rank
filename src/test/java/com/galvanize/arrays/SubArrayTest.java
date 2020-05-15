b42a9ac94e8fa292db42b83c10af707dc436e37a
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-05-15 00:58:50 -0700

    setup oice

---
+++ src/test/java/com/galvanize/arrays/SubArrayTest.java
@@ -0,0 +1,20 @@
+package com.galvanize.arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubArrayTest {
+    //Alex Lundin
+    //OICE - An effective way to consume requirements
+    //Output
+    //  print count of negative sub arrays
+    //Input
+    //  array of n integers
+    //Constraints
+    //  Are there any stated constraints to the problem?
+    //  Are there any indicators of time vs space trade-offs?
+    //Examples and Edge Cases
+    //  List examples from problem
+    //  Think of edge cases at boundary values, 0 and 1 are typical
+
+
+}
