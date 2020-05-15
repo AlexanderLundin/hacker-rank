a3386024447837a751d773edd3419eec611c9b96
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-05-15 09:50:02 -0700

    first array passing

--- src/main/java/com/galvanize/arraylist/ChallengeArrayList.java
+++ src/main/java/com/galvanize/arraylist/ChallengeArrayList.java
@@ -1,7 +1,36 @@
 package com.galvanize.arraylist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ChallengeArrayList {
 
+    public static Integer[][] toArrays(ArrayList arrayList){
+        int i = 0;
+        int arrayCount = 0;
+        Integer n = (int) arrayList.get(i);
+        int length;
+        int d;
+        Integer arrays [][] = new Integer [++n][];
+        arrays[arrayCount] = new Integer[]{--n};
+        arrayCount++;
+        Integer array [];
+        for (i = 1; i < n ; i++) {
+            d = (Integer) arrayList.get(i);
+            length = d;
+            array = new Integer [++length];
+            array[0] = d;
+            i++;
+            for (int j = 2; j < length + 1 ; j++) {
+                array[j - 1] = (Integer) arrayList.get(i);
+                i++;
+            }
+            arrays[arrayCount] = array;
+            arrayCount++;
+        }
+        return arrays;
+    }
 
     public static void getValueAtXY(Integer[][] arrays, int x, int y) {
         Integer array[];
@@ -12,5 +41,38 @@ public class ChallengeArrayList {
             String value = Integer.toString(array[y]);
             System.out.println(value);
         }
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        arrayList.add(n);
+        int d = 0;
+        int value = 0;
+        for (int i = 0; i < n ; i++) {
+            d = scan.nextInt();
+            arrayList.add(d);
+            for (int j = 0; j < d ; j++) {
+                value = scan.nextInt();
+                arrayList.add(value);
+            }
+        }
+        // loop for queries
+        n = scan.nextInt();
+        arrayList.add(n);
+        int q = 0;
+        for (int i = 0; i < n ; i++) {
+            q = scan.nextInt();
+            arrayList.add(q);
+            for (int j = 0; j < d ; j++) {
+                value = scan.nextInt();
+                arrayList.add(value);
+            }
+        }
+
     }
 }
--- src/test/java/com/galvanize/arraylist/ChallengeArrayListTest.java
+++ src/test/java/com/galvanize/arraylist/ChallengeArrayListTest.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.TestInstance;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-
 class ChallengeArrayListTest {
     //Alex Lundin
     //OICE - An effective way to consume requirements
@@ -31,16 +33,17 @@ class ChallengeArrayListTest {
     //Examples and Edge Cases
     //  List examples from problem
     //  Think of edge cases at boundary values, 0 and 1 are typical
-        //Integer array1[] = {5, 41, 77, 74, 22, 44};
-        //Integer array2[] = {1, 12};
-        //Integer array3[] = {4, 37, 34, 36, 52};
-        //Integer array4[] = {0};
-        //Integer array5[] = {3, 20, 22, 33};
-        //List<Integer> myList1 = Arrays.asList(array1);
-        //List<Integer> myList2 = Arrays.asList(array2);
-        //List<Integer> myList3 = Arrays.asList(array3);
-        //List<Integer> myList4 = Arrays.asList(array4);
-        //List<Integer> myList5 = Arrays.asList(array5);
+    //Integer array0[] = {2};
+    //Integer array1[] = {5, 41, 77, 74, 22, 44};
+    //Integer array2[] = {1, 12};
+    //Integer array3[] = {4, 37, 34, 36, 52};
+    //Integer array4[] = {0};
+    //Integer array5[] = {3, 20, 22, 33};
+    //List<Integer> myList1 = Arrays.asList(array1);
+    //List<Integer> myList2 = Arrays.asList(array2);
+    //List<Integer> myList3 = Arrays.asList(array3);
+    //List<Integer> myList4 = Arrays.asList(array4);
+    //List<Integer> myList5 = Arrays.asList(array5);
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
@@ -52,11 +55,11 @@ class ChallengeArrayListTest {
     public void setup() {
         //Setup
         System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream (errContent));
+        System.setErr(new PrintStream(errContent));
     }
 
     @AfterAll
-    public void restoreStreams(){
+    public void restoreStreams() {
         System.setOut(originalOut);
         System.setErr(originalErr);
     }
@@ -81,14 +84,39 @@ class ChallengeArrayListTest {
     public void getValueAtXY_printsError() {
         //Setup
         String expected = "ERROR!";
-        Integer array0[] = {2};
-        Integer array1[] = {5};
-        Integer array2[] = {1};
-        Integer arrays[][] = {array0, array1, array2};
+        Integer[] array0 = {2};
+        Integer[] array1 = {5};
+        Integer[] array2 = {1};
+        Integer[][] arrays = {array0, array1, array2};
         //Exercise
         ChallengeArrayList.getValueAtXY(arrays, 1, 3);
         //Assert
         assertThat(outContent.toString(), containsString(expected));
+        //Teardown
+    }
+
+    @Test
+    public void toArrays_returnsArrays() {
+        //Setup
+        Integer[] array0 = {5};
+        Integer[] array1 = {5, 41, 77, 74, 22, 44};
+        Integer[] array2 = {1, 12};
+        Integer[] array3 = {4, 37, 34, 36, 52};
+        Integer[] array4 = new Integer[]{0};
+        Integer[] array5 = {3, 20, 22, 33};
+        Integer[][] arrays = {array0, array1, array2, array3, array4, array5};
+
+        int[] ints = {5, 5, 41, 77, 74, 22, 44, 1, 12, 4, 37, 34, 36, 52, 0, 3, 20, 22, 33};
+        ArrayList arrayList = new ArrayList<Integer>(ints.length);
+        for (int i : ints)
+        {
+            arrayList.add(i);
+        }
+        //Exercise
+
+        Integer[][] actual = ChallengeArrayList.toArrays(arrayList);
+        //Assert
+        assertEquals(arrays, actual);
         //Teardown
     }
 }
